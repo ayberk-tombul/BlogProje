37fd5e09d9dfa632bafb5fd72c1933fea45f6b67
AYBERK TOMBUL <ayberktombul@gmail.com>
2022-10-14 13:45:51 +0300

    Fix

---
+++ BlogProje/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using BlogProje.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlogProje.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    UserName = "Ayberk"
+                },
+                new UserComment
+                {
+                    ID= 2,
+                    UserName="Mesut"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    UserName="Merve"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
